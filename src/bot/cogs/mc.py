a87de79ef1079e9edc5e11091cc28245bb2d50df
Nater0214 <gamer.nater0214@gmail.com>
2023-06-12 02:20:43 -0400

    bruh

--- src/bot/cogs/mc.py
+++ src/bot/cogs/mc.py
@@ -30,6 +30,23 @@ class Minecraft(commands.Cog):
             subprocess.run(["/var/mc-servers/blox_smp_1/start.sh", "y"])
     
     
+    @commands.slash_command(help="Starts a minecraft server")
+    async def listmc(self, interaction: Interaction, server: str):
+        """Starts a minecraft server"""
+        
+        # Check for valid server name
+        if not server in ["blox-smp"]:
+            await interaction.response.send_message("Invalid server name")
+            return
+        
+        # Send status message
+        await interaction.response.send_message(f"Starting minecraft server {server}...")
+        
+        # Start the server
+        if server == "blox-smp":
+            subprocess.run(["/var/mc-servers/blox_smp_1/start.sh", "y"])
+    
+    
     @commands.slash_command(help="Lists all minecraft servers")
     async def listmc(self, interaction: Interaction):
         """Lists all minecraft servers"""
