f5f9e571d169612232faf4dea7a4deb27e22bc76
Nater0214 <gamer.nater0214@gmail.com>
2023-06-20 20:00:41 -0400

    songs are able to be found

--- lib/kmaudio/src/get_audio.py
+++ lib/kmaudio/src/get_audio.py
@@ -4,16 +4,41 @@
 
 # Imports
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 # Definitions
-def get_audio(query: str):
-    """Get a Kevin audio by a search query"""
+class GetAudio:
+    """Holding stuff"""
     
-    # Initialize webdriver
-    options = webdriver.Chrome.Options()
-    for a in {'--log-level=1', '--headless'}: options.add_argument(a)
-    driver = webdriver.Chrome(options=options)
+    # Init
+    def __init__(self) -> None:
+        """Initialize the 'function'"""
+        
+        # Initialize webdriver
+        options = webdriver.chrome.options.Options()
+        for a in ['--log-level=1', '--headless']: options.add_argument(a)
+        self.driver = webdriver.Chrome(options=options)
     
-    # Get the music webpage
-    driver.get("https://incompetech.com/music/royalty-free/music.html")
+    def __call__(self, query: str):
+        """Get a Kevin audio by a search query"""
+        
+        # Get the music webpage
+        self.driver.get("https://incompetech.com/music/royalty-free/music.html")
+        
+        # Get search bar element
+        search_bar_elem = self.driver.find_element(By.ID, "incompetechSearchSearchText")
+        
+        # Input the query
+        search_bar_elem.send_keys(query)
+        
+        # Get song list element
+        song_elems = self.driver.find_elements(By.CLASS_NAME, "search-result-row")
+        
+        # Get the song names
+        song_names = [elem.find_element(By.TAG_NAME, "b").text for elem in song_elems]
+        
+        # Return song names
+        return song_names
+
+get_audio = GetAudio()
