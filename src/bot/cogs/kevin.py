a8099a4ca4a601b81a96029616b5955156a6b419
Nater0214 <gamer.nater0214@gmail.com>
2023-06-22 02:46:37 -0400

    Searching now completely works

--- src/bot/cogs/kevin.py
+++ src/bot/cogs/kevin.py
@@ -3,7 +3,7 @@
 
 
 # Imports
-from discord import Interaction, Option, FFmpegPCMAudio
+from discord import ApplicationContext, Option
 from discord.ext import commands
 
 from lib import kmaudio
@@ -18,25 +18,33 @@ class Kevin(commands.Cog):
     
     # Commands
     @commands.slash_command(help="Search for and play a Kevin Macleod song by a query")
-    async def pkm(self, ctx: commands.Context, query: Option(str)):
+    async def pkm(self, interaction: ApplicationContext, query: Option(str)):
         """Play a Kevin Macleod song by a query"""
         
         # Join the sender's voice channel if the bot isn't already in one
-        if ctx.voice_client is None:
-            if ctx.author.voice.channel is not None:
-                vc = await ctx.author.voice.channel.connect()
+        if interaction.guild.voice_client is None:
+            if interaction.user.voice is not None:
+                vc = await interaction.user.voice.channel.connect()
             else:
-                await ctx.reply("You aren't in a VC dumbass")
+                await interaction.response.send_message("You aren't in a VC dumbass")
                 return
         else:
-            await ctx.reply("I'm busy rn sorry")
+            await interaction.response.send_message("I'm busy rn sorry")
             return
         
+        # Acknowledge the command
+        await interaction.defer()
+        
+        # Get the query results
         query_results = kmaudio.search_song(query)
+        
+        # Respond appropriately
         if len(query_results) > 1:
-            lf = '\n'
-            ctx.reply(f"I found {len(query_results)} songs:{lf}{lf.join([f'{num}. {song}' for num, song in enumerate(query_results)])}")
+            await interaction.followup.send("\n".join([f"I found {len(query_results)} songs:", *[f'{num}. {song}' for num, song in enumerate(query_results)]]))
         elif len(query_results) == 1:
-            ctx.reply(f"I found {query_results[0]}")
+            await interaction.followup.send(f"I found {query_results[0]}")
         else:
-            ctx.reply("I found nothing :/")
+            await interaction.followup.send("I found nothing :/")
+        
+        # Leave the voice channel
+        await vc.disconnect()
