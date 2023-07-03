34d89aa2f463d73a46e017f76ea7d226adbd4674
Nater0214 <gamer.nater0214@gmail.com>
2023-07-03 15:05:14 -0400

    Sort imports

--- lib/audio/_search_song.py
+++ lib/audio/_search_song.py
@@ -4,8 +4,8 @@
 
 # Imports
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 
